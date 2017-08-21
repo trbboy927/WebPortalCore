77a57b2acbf466a7064075349495dd879ff17ba5
trbboy927 <trbboy927@gmail.com>
2017-08-21 22:02:55 +0800

    上傳

---
+++ WebPortalCore/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPortalCore.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
